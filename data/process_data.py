4cc16154da898a32bf98a024776614f518141446
M <markus.stachl@googlemail.com>
2022-03-12 01:11:35 +0100

    finish process_data.py and train_classifier.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,97 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    Load input datafiles, join them and return a concatenated dataframe.
+
+    Parameters
+    ----------
+    messages_filepath : STR
+        PATH TO THE FIRST INPUT FILE (CSV).
+    categories_filepath : TYPE
+        PATH TO THE SECOND INPUT FILE (CSV).
+
+    Returns
+    -------
+    df : DATAFRAME
+        DATAFRAME CONCATENATING DATA FROM BOTH INPUT FILES.
+
+    """
+    # load message file into dataframe
+    messages = pd.read_csv(messages_filepath, index_col='id')
+    # load categories file into dataframe
+    categories = pd.read_csv(categories_filepath, index_col='id')
+    # join the two dataframes
+    df = messages.join(categories)
+    return df
 
 
 def clean_data(df):
-    pass
+    """
+    Perform cleaning operations on the dataframe. This includes handling
+    categorical variables, dropping duplicates and formatting category columns.
+
+    Parameters
+    ----------
+    df : DATAFRAME
+        DATAFRAME TO BE CLEANED.
+
+    Returns
+    -------
+    df : DATAFRAME
+        CLEANED DATAFRAME.
+
+    """
+    # create a dataframe of the 36 individual category columns
+    categories = df.categories.str.split(';',expand=True)
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = [x.split('-')[0] for x in row]
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].astype(str).str[-1]
+    
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+    
+    # drop the original categories column from `df`
+    df = df.drop(columns=["categories"])
+    df = df.join(categories)
+    
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """
+    Stores a given dataframe into a sql database. The table name is set to 
+    'Messages'.
+
+    Parameters
+    ----------
+    df : DATAFRAME
+        DATAFRAME TO BE STORED.
+    database_filename : STR
+        TARGET FILENAME OF THE SQLITE DB.
+
+    Returns
+    -------
+    None.
+
+    """
+    sqlite_db = 'sqlite:///' + database_filename
+    engine = create_engine(sqlite_db)
+    df.to_sql('Messages', engine, index=False)  
 
 
 def main():
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,171 @@
 import sys
+import pandas as pd
+import re
+
+from nltk.tokenize import word_tokenize
+from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize 
+from nltk.corpus import stopwords 
+from nltk.stem import WordNetLemmatizer
+from sklearn.pipeline import Pipeline 
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import classification_report
+import nltk
+import pickle
+nltk.download(['punkt','stopwords','wordnet'])
 
 
 def load_data(database_filepath):
-    pass
+    """
+    Load sqlite database file and split it into independent variable X 
+    (message) and dependent variable Y (categories).
+
+    Parameters
+    ----------
+    database_filepath : STR
+        PATH TO DATABASE FILE.
+
+    Returns
+    -------
+    X : DATAFRAME
+        MESSAGE DATAFRAME.
+    Y : DATAFRAME
+        DESCRIPTION.
+    category_names : DATAFRAME
+        CATEGORIES DATAFRAME.
+
+    """
+    # Set up db engine
+    sqlite_db = 'sqlite:///' + database_filepath
+    engine = create_engine(sqlite_db)
+    
+    # Connect to database and load Message table into Dataframe
+    df = pd.read_sql_table('Messages', con = engine.connect())
+    
+    # Extract category names from the dataframe
+    category_names = list(df.columns)[4:]
+    
+    # Extract dependent and independent variables
+    X = df['message']  # Message Column
+    Y = df[category_names] # Classification label
+    
+    return X,Y,category_names
 
 
 def tokenize(text):
-    pass
+    """
+    Split text into words and return the root form of the words
+
+    Parameters
+    ----------
+    text : STR
+        THE MESSAGE.
+
+    Returns
+    -------
+    lemm : LIST(STR)
+        A LIST OF THE ROOT FORM OF MESSAGE WORDS.
+
+    """
+    # Normalize text
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    
+    # Tokenize text
+    words = word_tokenize(text)
+    
+    # Remove stop words
+    stop = stopwords.words("english")
+    words = [t for t in words if t not in stop]
+    
+    # Lemmatization
+    lemm = [WordNetLemmatizer().lemmatize(w) for w in words]
+    
+    return lemm
 
 
 def build_model():
-    pass
+    """
+    Build a pipeline with a Bag-of-Words Wordcounter, TF-IDF and Adaboost 
+    Classifier parameterized using parameters obtained from GridSearch.
+
+    Returns
+    -------
+    gridsearch_pipeline : GRIDSEARCHCV
+        MODEL CONTAINING NLP PIPELINE.
+
+    """
+    # Create pipeline with Bag-of-Words, TF-IDF and Adaboost Classifier
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer = tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf',  MultiOutputClassifier(AdaBoostClassifier()))
+        
+    ])
+    
+    # Create Grid search parameters for Adaboost Classifier   
+    parameters = {
+        'tfidf__use_idf': (True, False),
+        'clf__estimator__n_estimators': [10,20,50],
+    }
+
+    gridsearch_pipeline = GridSearchCV(pipeline, param_grid = parameters)
+    return gridsearch_pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """
+    Predict model results on test set. Print F1-score, precision and recall
+    for each feature and print overall accuracy on test values.
+
+    Parameters
+    ----------
+    model : GRIDSEARCHCV
+        THE NLP PIPELINE (MODEL).
+    X_test : DATAFRAME
+        THE TEST MESSAGES.
+    Y_test : DATAFRAME
+        THE TEST CATEGORIES.
+    category_names : LIST(STR)
+        LIST OF CATEGORIES, EQUAL TO COLUMN NAMES OF Y_TEST.
+
+    Returns
+    -------
+    None.
+
+    """
+    # Prediction with Adaboost Classifier  
+    y_pred = model.predict(X_test)
+    # Plot classification reports(f1-score, precision, recall)  for each feature
+    for idx, col in enumerate(Y_test):
+        print('Feature: {}'.format(col))
+        print(classification_report(Y_test[col], y_pred[:, idx]))
+    # compute and plot model accuracy   
+    accuracy = (Y_test.values == y_pred).mean()       
+    print('Model accuracy: {}'.format(accuracy))    
 
 
 def save_model(model, model_filepath):
-    pass
+    """
+    Exports the model as a pickle file.
+
+    Parameters
+    ----------
+    model : ESTIMATOR
+        THE ADABOOST MODEL.
+    model_filepath : STR
+        DESIRED FILE LOCATION FOR STORED MODEL.
+
+    Returns
+    -------
+    None.
+
+    """
+    # Export model as pickle file
+    with open (model_filepath, 'wb') as file:
+        pickle.dump(model, file)
 
 
 def main():
